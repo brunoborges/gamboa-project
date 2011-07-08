e1da9d596f7ea1b574b60680f9e78efb083d9f97
Bruno Borges <bruno.borges@gmail.com>
2011-07-08 16:29:14 -0300

    added spring injector

--- gamboa-clean-archetype/src/main/resources/archetype-resources/code/Application.scala
+++ gamboa-clean-archetype/src/main/resources/archetype-resources/code/Application.scala
@@ -1,9 +1,13 @@
 package code
 
 import org.apache.wicket.protocol.http.WebApplication
-import code.pages.Index
+import code.pages._
+import org.apache.wicket.spring.injection.annot.SpringComponentInjector
 
 class Application extends WebApplication {
+
+  getComponentInstantiationListeners().add(new SpringComponentInjector(this))
+  getMarkupSettings().setStripWicketTags(true)
 
   def getHomePage = classOf[Index]
 
